aeca67b6ac732bac6f72cb9e1ce3138455c72989
Brandon Vergara <brandon@makingdevs.com>
2018-05-17 23:49:18 -0500

    adding elm to brunch

--- brunch-config.js
+++ brunch-config.js
@@ -10,5 +10,35 @@ exports.files = {
 };
 
 exports.plugins = {
-  babel: {presets: ['latest']}
+  babel: {presets: ['latest']},
+  elmBrunch: {
+    // (required) Set to the elm file(s) containing your "main" function `elm make` 
+    //            handles all elm dependencies relative to `elmFolder`
+    mainModules: ['source/path/YourMainModule.elm'],
+
+    // (optional) Set to keep an independent elm-stuff folder per mainModule. If 
+    //            mainModules is an array, then the 'source/path' of each mainModule 
+    //            will be appended to your elm-folder, allowing each subfolder to keep their
+    //            own set of elm dependencies.
+    independentModules: true,
+
+    // (optional) Set to path where `elm-make` is located, relative to `elmFolder`
+    executablePath: '../../node_modules/elm/binwrappers',
+
+    // (optional) Set to path where elm-package.json is located, defaults to project root
+    //            if your elm files are not in /app then make sure to configure 
+    //            paths.watched in main brunch config
+    elmFolder: 'path/to/elm-files',
+
+    // (optional) Defaults to 'js/' folder in paths.public
+    // relative to `elmFolder`
+    outputFolder: 'some/path/',
+
+    // (optional) If specified, all mainModules will be compiled to a single file 
+    //            This is merged with outputFolder.
+    outputFile: 'elm.js',
+
+    // (optional) add some parameters that are passed to elm-make
+    makeParameters: ['--warn']
+  }
 };
